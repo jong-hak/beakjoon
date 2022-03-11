bb05feef50a064cfd6fe0a2045d4713fb6cf0dd8
jonghak <76627237+jong-hak@users.noreply.github.com>
2022-03-11 20:58:55 +0900

    Update 7869：두 원.cpp

--- 기하/7869：두 원.cpp
+++ 기하/7869：두 원.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<algorithm>
 #include<math.h>
-#include<string.h>
 #define fastio (std::ios_base::sync_with_stdio(false), std::cin.tie(NULL), std::cout.tie(NULL))
 #define Pi 3.14159265359
 
@@ -12,23 +11,22 @@ int main()
 	std::cin >> x1 >> y1 >> r1 >> x2 >> y2>> r2;
 	double A, B;
 	double d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	double ans;
 	if (d >= r1 + r2) {
-		double a = 0;
-		std::cout << std::fixed;
-		std::cout.precision(3);
-		std::cout << a;
+		ans = 0;
 	}
 	else if (d + r1 <= r2){
-		std::cout << r1 * r1 * Pi;
+		ans= r1 * r1 * Pi;
 	}
 	else if (d + r2 <= r1) {
-		std::cout << r2 * r2 * Pi;
+		ans =  r2 * r2 * Pi;
 	}
 	else {
 		A = acos((r1 * r1 + d * d - r2 * r2) / (2 * r1 * d));
 		B = acos((r2 * r2 + d * d - r1 * r1) / (2 * r2 * d));
-		std::cout << std::fixed;
-		std::cout.precision(3);
-		std::cout << r2 * r2 * B + r1 * r1 * A - r1 * sin(A) * d;
+		ans = r2 * r2 * B + r1 * r1 * A - r1 * sin(A) * d;
 	}
+	std::cout << std::fixed;
+	std::cout.precision(3);
+	std::cout << ans;
 }
